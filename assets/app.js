b05291e1cd6d8fdd43f51c43c03d52b859a8ccf7
Alisa Zamkova <alisazamkovaya@gmail.com>
2026-07-16 00:09:55 +0200

    Code cleanup and project validation

--- assets/app.js
+++ assets/app.js
@@ -7,8 +7,6 @@ import './stimulus_bootstrap.js';
  */
 import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
-
 document.addEventListener('DOMContentLoaded', () => {
     const mainImage = document.querySelector('#mainMenuImage');
     const thumbnails = document.querySelectorAll('.js-menu-thumb');
